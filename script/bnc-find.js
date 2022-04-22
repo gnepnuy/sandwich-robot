d02de80957f796fe9094736d54d24aaac5c64a84
yunpeng <zhuyunpeng@aliyun.com>
2022-04-22 14:23:36 +0800

    update

--- script/bnc-find.js
+++ script/bnc-find.js
@@ -39,8 +39,8 @@ let router = new ethers.Contract(routerAddess,Router.abi,walletWithProvider);
 
 // create options object
 const options = {
-  // dappId: 'd3da6c32-5300-4f22-af99-f574da5843fe',
-  dappId: '0d306f4c-3137-4c41-88f9-e618d8c7de98',
+  dappId: 'd3da6c32-5300-4f22-af99-f574da5843fe',
+  // dappId: '0d306f4c-3137-4c41-88f9-e618d8c7de98',
   networkId: 56,
   // networkId: 5,
   transactionHandlers: [event => recordTx(event.transaction)],
@@ -153,7 +153,7 @@ function recordTx(transaction){
     //console.log('current time:',new Date().getTime());
     if(transaction.status == 'pending'){
         txInfo.set(transaction.hash,transaction);
-        console.log('find new pending transaction，hash:',transaction.hash);
+        //console.log('find new pending transaction，hash:',transaction.hash);
     }else{
         if(txInfo.has(transaction.hash)){
             txInfo.delete(transaction.hash);
@@ -163,94 +163,32 @@ function recordTx(transaction){
     //console.log('--------------------------------------------------------------------')
 }
 
-var lastSendTime = 0
+
 
 async function handlerPengdingTx(transaction){
-  
-  const receivePendingTime = new Date().getTime()/1000;
-  // console.log("Transaction is pending:",JSON.stringify(transaction))
+
   const paths = transaction.contractCall.params.path;
   if(paths.length > 2){
     return
   }
-  // console.log('===================path:',paths);
-  const toCoin = paths[paths.length-1];
-
-
   // console.log(transaction)
   //判断是否为目标币种
-  if(targetCoins.coins.join().indexOf(paths[1]) > 0){
+  if(targetCoins.coins.join().indexOf(paths[1]) < 0){
      // //计算出当前交易的滑点
     const amountIn = transaction.value;
-    // console.log('amountIn:',amountIn)
-
-    const beforeQueryTime = new Date().getTime()/1000;
-
-    //间隔交易
-    if((beforeQueryTime - 5) < lastSendTime){
-      return
-    }
-
     //排除小额交易
-    console.log('value:',transaction.value/1000000000000000000)
     if(transaction.value < 500000000000000000){
       return
     }
     const amountOut = await router.getAmountsOut(amountIn,paths);//查询当前能换的数量
-    const afterQueryTime = new Date().getTime()/1000;
 
 
     const expectAmountOut = amountOut[paths.length - 1].toString();//当前能换的数量  
     const minAmountOut = transaction.contractCall.params.amountOutMin;//设定的最少能换的数量
 
     const slippage = ((expectAmountOut - minAmountOut) / expectAmountOut) * 100;
-    if(slippage > 5){
-      //计算出盈利空间
-      //这里直接使用目标交易的数量和滑点的大小来确定我们要使用的资金量
-      let maxMultiple = Math.floor((slippage/0.5) - 2);//最大倍数减去2，试试
-      if(maxMultiple > 3){
-        maxMultiple =3
-      }
-      
-      //组装交易
-      const maxFeePerGas = transaction.gasPrice;
-      const standardGas = 5000000000;
-      let sellGas = standardGas;
-      if(maxFeePerGas > sellGas){
-        sellGas = maxFeePerGas -1;
-      }
-      let overrides = {
-        gasPrice: standardGas *3,
-        gasLimit: 150816,
-      };
-      console.log('amountIn:',amountIn,',maxMultiple:',maxMultiple,',toCoin:',toCoin);
-      // const amountIn = 1000000000000000;
-      // const maxMultiple = 10;
-
-      if(txInfo.has(transaction.hash)){
-        const beforeSendTime = new Date().getTime()/1000;
-        // walletWithProvider = new ethers.Wallet(privateKey, officialProvider);
-        // wallet2 = new ethers.Wallet(privateKey2, officialProvider);
-        sandwichBot.connect(walletWithProvider).buy(amountIn,maxMultiple,toCoin,0,overrides);
-
-        overrides = {
-          gasPrice: sellGas,
-          gasLimit: 130816,
-        };
-        sandwichBot.connect(wallet2).sell(toCoin,overrides);
-        const afterSendTime = new Date().getTime()/1000;
-
-
-        console.log('--------------------------------',transaction.hash,'--------------------------------------')
-        console.log(
-          'beforeQueryTime:',beforeQueryTime,
-          'afterQueryTime:',afterQueryTime,
-          'beforeSendTime:',beforeSendTime,
-          'afterSendTime:',afterSendTime
-          )
-        lastSendTime = afterSendTime;
-      }
-    }
+    console.log('slippage:',slippage,'% ,hash:',transaction.hash)
+    
   }
  
     
